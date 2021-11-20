423b6b13ef5e3f18dbf51e697b7451911cbc11a6
Senthilnathan <ssenthil1490@outlook.com>
2021-11-20 17:58:19 +0530

    "add binary tree traversal"
    Signed-off-by: Senthilnathan <ssenthil1490@outlook.com>

---
+++ binary_tree/bn_tree.py
@@ -0,0 +1,36 @@
+class Tree:
+    def __init__(self, data):
+        self.key = data
+        self.left = None
+        self.right = None
+
+
+def inOrder(root):
+    if root:
+        preOrder(root.left)
+        print(root.key)
+        preOrder(root.right)
+
+def preOrder(root):
+    if root:
+        print(root.key)
+        preOrder(root.left)
+        preOrder(root.right)
+
+def postOrder(root):
+    if root:
+        postOrder(root.left)
+        postOrder(root.right)
+        print(root.key)
+
+root = Tree(1)
+root.left = Tree(2)
+root.right = Tree(3)
+root.left.left = Tree(4)
+root.left.right = Tree(5)
+print("Inorder: ")
+inOrder(root)
+print("\nPost: ")
+postOrder(root)
+print("\nPre: ")
+preOrder(root)
